8ceed71178dd4b955251422e6225b28124a744e3
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2020-07-08 17:42:37 +0200

    adds more comments to example_predict_thresholds.py

--- examples/example_predict_thresholds.py
+++ examples/example_predict_thresholds.py
@@ -5,6 +5,13 @@ Runs MLDS using the mlds wrapper in python (which calls R in the background)
 and predicts the 2AFC thresholds at given standard values.
 Uses bootstrap samples from MLDS to also provide thresholds with bootstrap CIs
 
+It saves threhsold data in a JSON. 
+
+Check out the following references for more details.
+
+[1] Aguilar, Wichmann & Maertens (2017) Journal of Vision.
+[2] Devinck & Knoblauch (2012) Journal of Vision.
+
 @author: G. Aguilar, April 2020
 """
 
@@ -17,20 +24,38 @@ sns.set_context('talk')
 sns.set_style("ticks", {'xtick.direction': 'in', 'ytick.direction': 'in',
                         "xtick.major.size": 2, "ytick.major.size": 2})
 
+# raw csv file of MLDS data
 name = 'data.csv'
 
-# parameters for prediction form a scale
-dp = [-2, -1, -0.5, 0.5, 1, 2]  # d' around st to be predicted from scale
+#### parameters for prediction from a perceptual scale
+
+# interpolation of the perceptual scale
 k = 3  # spline degree, 3 for cubic spline
-factor = 2.0  # factor correction
+
+# factor multiplication to  transform perceptual scale in units of d'
+# assuming an internal representation that is a gaussian random variable with
+# variance sigma, the decision variable in the MLDS model will also be 
+# be a gaussian random variable with variance 4*sigma. MLDS estimates the overall 
+# variance \hat{sigma} of the decision variable, and the inverse is the 
+# the scale's height. Thus, to transform the height to units of the *internal*
+# representation, we multiply by 2. (more detailed derivation in Appendix of [1])
+factor = 2.0  
+
+#
+nsamples = 1000  # number of bootstrap samples 
+res = 0.000015  # interpolation resolution. decrease if needed.
+
+# Standards: at which stimulus levels you want to predict thresholds?
+sts = np.array([0.25, 0.5, 0.75]) 
+# d' : thresholds will be derived to the following d' performance 
+dp = [-2, -1, -0.5, 0.5, 1, 2]  # d' around the  standard at which we will read out thresholds
+# (negative values indicate comparisons below the standard, positive values for 
+# comparisons above the standard)
+
+
+# type of confidence interval. 
 citype = 'BCa'  # bias-corrected and accelerated CIs
 # citype='percentile'  # raw percentile CI
-
-nsamples = 1000  # number of bootstrap samples 
-res = 0.000015
-
-# standards
-sts = np.array([0.0303])  # , 0.0466, 0.0680])
 
 # %% Estimating scale calling MLDS
 ml = mlds.MLDSObject(name, boot=True, standardscale=False, verbose=False)
