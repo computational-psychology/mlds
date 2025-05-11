63708a4efc4afb2b4a258f826a036cd153968ef9
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2025-05-11 13:26:02 +0200

    update README with example, and adds visualization

--- examples/example.py
+++ examples/example.py
@@ -10,38 +10,43 @@ data.csv was created by simulation.
 import matplotlib.pyplot as plt
 import mlds
 
+plt.style.use('seaborn-v0_8-notebook')
 
 ## creating MLDS object and running analysis
-obs = mlds.MLDSObject('data_triads.csv', standardscale=False, 
-                      boot=True, verbose=False)
+obs = mlds.MLDSObject('data_triads.csv',
+                      standardscale=False, # standard scale normalizes range from 0 to 1
+                      boot=True) # to calculate confidence intervals via bootstrap
 obs.nsamples = 1000
 
 print("Running analysis...")
-# the wrapper now sends the commands to R, and R runs the fitting. 
+# the wrapper now sends the commands to R, and R runs the fitting.
 obs.run()  # always runs the analysis, overwrites the .MLDS file if exists.
 
-# The wrapper makes R save everything (scale values, bootstrap CIs etc) 
-# in a .MLDS file. This file can be opened manually with R itself, or 
-# used by this wrapper to plot and get useful information about 
+# The wrapper makes R save everything (scale values, bootstrap CIs etc)
+# in a .MLDS file. This file can be opened manually with R itself, or
+# used by this wrapper to plot and get useful information about
 # the fitted model (see below).
 
 # Instead of run(), one can also call .load()
 #obs.load()   # loads results from .MLDS file and does not run the analysis again.
 
-# This option can be useful as bootstrapping takes long. If there is no change in our 
-# data, we can call run() only once, and later just call load() to read the data 
+# This option can be useful as bootstrapping takes long. If there is no change in our
+# data, we can call run() only once, and later just call load() to read the data
 # from the previously saved MLDS file. If the MLDS file does not exist,
 #  load() will automatically call run().
 
 
 ## plotting the scale
 print("plotting...")
-plt.figure()
-plt.errorbar(obs.stim, obs.mns, yerr=abs(obs.ci95 - obs.mns), color='#4C72B0',
+fig, ax = plt.subplots(1, 1, figsize=(5, 4))
+ax.errorbar(obs.stim, obs.scale, yerr=abs(obs.ci95 - obs.mns), color='#4C72B0',
              linewidth=2, elinewidth=2)
 
-plt.xlabel('Stimulus')
-plt.ylabel('Difference Scale')
+ax.set_xlabel('Stimulus')
+ax.set_ylabel('Perceptual scale')
+ax.spines[['right', 'top']].set_visible(False)
+plt.tight_layout()
+fig.savefig('example_triads.png')
 plt.show()
 
 
